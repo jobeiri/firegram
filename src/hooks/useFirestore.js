7b8d6c32199ea145d77fedd7b5b6b7d841700571
Amir Hossein Jobeiri <jobeiri_a.h@yahoo.com>
2020-10-28 03:28:48 -0700

    Firestore hook & showing images

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { projectFirestore } from "../firebase/config";
+
+const useFirestore = (collection) => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const unSubscribe = projectFirestore
+      .collection(collection)
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        let documents = [];
+        snapshot.forEach((doc) => {
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+        setDocs(documents);
+      });
+
+    return () => unSubscribe();
+  }, [collection]);
+
+  return { docs };
+};
+
+export { useFirestore as default };
